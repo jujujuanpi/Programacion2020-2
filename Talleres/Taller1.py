ffd465e540c6e168f7a6359db96e4fce2d4e0920
jujujuanpi <heyjuanpi@gmail.com>
2020-08-19 13:14:00 -0500

    Terminación Taller 1 2020/08/19

--- Talleres/Taller1.py
+++ Talleres/Taller1.py
@@ -5,19 +5,20 @@ print ("Bienvenido al calculador de IMC. Por favor, ingrese los siguientes datos
 Nombre = input("Ingrese su nombre: ")
 Peso = float(input("Ingresar su peso en kilogramos: "))
 Estatura = float(input("Ingresar su estatura: "))
-IMC = Peso / Estatura**2
+IMC = round(Peso / Estatura**2,3)
 
 print (Nombre, Peso, Estatura)
+print (IMC)
 
 if (IMC < 18.5):
-    print ("El paciente {Nombre} se encuentra en un estado de infrapeso.")
+    print (f"El paciente {Nombre} se encuentra en un estado de infrapeso.")
 elif (IMC >= 18.5 and IMC < 24.9):
-    print ("El panciente {Nombre} se encuentra con un peso normal.")
+    print (f"El panciente {Nombre} se encuentra con un peso normal.")
 elif (IMC >= 25 and IMC < 30):
-    print ("El panciente {Nombre} se encuentra con sobrepeso.")
+    print (f"El panciente {Nombre} se encuentra con sobrepeso.")
 elif (IMC >= 30 and IMC < 35):
-    print ("El panciente {Nombre} presenta un estado de obesidad.")
+    print (f"El panciente {Nombre} presenta un estado de obesidad.")
 else:
-    print ("El paciente {Nombre} presenta obesidad mórbida.")
+    print (f"El paciente {Nombre} presenta obesidad mórbida.")
 
 print ("Gracias por participar.")
